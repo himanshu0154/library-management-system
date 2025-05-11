0582bd20285fad1f9b46bebd2e2570a80574cd9b
himanshu0154 <himanshusingh.pv@gmail.com>
2025-05-12 00:33:10 +0530

    fixed bugs

--- main.py
+++ main.py
@@ -2,22 +2,22 @@ import sys
 
 class Library:
     def __init__(self):
-        self.list_of_books = []
-        self.books_record = []
-        self.borrowed_books = []
+        self.list_of_books = {}
+        self.borrowed_books = {}
+
 
 
     def add_books(self, no_of_books_add):
-        for i in range(1, no_of_books_add + 1):
-                name_of_books = input(f"{i}.  ")
-                if name_of_books.lower() in self.books_record:
+        for i in range(len(self.list_of_books) + 1, len(self.list_of_books) + no_of_books_add + 1):
+                name_of_books = input(f"{i}.  ").lower()
+                if any(book[0] == name_of_books for book in self.list_of_books.values()):
                     print("this book already exist")
-                    if name_of_books.lower() in self.borrowed_books: 
-                        print("it is currently being borrowed")
-                        continue
+                    break
+                    
                 else:
-                    self.list_of_books.append(name_of_books.lower())
-                    self.books_record.append(name_of_books.lower())
+                    self.list_of_books[i] = [name_of_books, "available"]
+
+                   
 
 
     def show_books(self):
@@ -25,56 +25,74 @@ class Library:
             print("there aren't any books available at the moment...")
         else:
             print("these are the books available - ")
-            for i, book in enumerate(self.list_of_books):
-                print(f"{i+1}. {book.title()}")
+            for i, book in self.list_of_books.items():
+                if book[1] == "borrowed":
+                    print(f"{i} - {book[0]}(borrowed)")
+                elif book[1] != "borrowed":
+                    print(f"{i} -  {book[0]}")
 
         
     def no_of_books_available(self):
-        print(f"there are {len(self.list_of_books)} available at the moment... and {len(self.borrowed_books)} are being borrowed")
+        available_count = 0
+        borrowed_count = 0
+        for i, book in self.list_of_books.items():
+            if book[1] == "available":
+                available_count += 1
+            else:
+                borrowed_count += 1
+
+        print(f"there are {available_count} books available at the moment... and {borrowed_count} are being borrowed")
 
 
     def borrow_books(self):
         if len(self.list_of_books) == 0:
-            print("there arent any books available for you to borrow")
+            print("there aren't any books available at the moment...")
         else:
-            print("these are the books available....")
-            for i, book in enumerate(self.list_of_books):
-                print(f"{i+1}. {book.title()}")
+            print("these are the books available - ")
+            for i, book in self.list_of_books.items():
+                if book[1] != "borrowed":
+                    print(f"{i} -  {book[0]}")
             no_of_borrow = int(input("enter the number of books you wanna borrow?\n"))
-            if no_of_borrow <= len(self.list_of_books):
+            available_books = [k for k, v in self.list_of_books.items() if v[1] == "available"]
+            if no_of_borrow <= len(available_books):
                 print("please enter the no. specific to the book you wanna borrow")
-                indexes = []
                 for i in range(1, no_of_borrow + 1):
-                    index = int(input("--> ")) - 1
-                    indexes.append(index)
+                    index = int(input("--> "))
+                    if index not in self.list_of_books:
+                        print("Invalid book number")
+                        continue
+                    elif self.list_of_books[index][1] == "borrowed":
+                        print(f"{self.list_of_books[index][0]} book is already borrowed")
+                    else:
+                        print(f"you have successfully borrowed {self.list_of_books[index][0]}")
+                        self.list_of_books[index][1] = "borrowed"
+                        self.borrowed_books[index] = [self.list_of_books[index][0], "borrowed"]
 
-                for index in sorted(indexes, reverse=True):
-                    book = self.list_of_books[index]
-                    print(f"you have successfully borrowed {book}")
-                    self.borrowed_books.append(book)
-                    self.list_of_books.remove(book)
             else:
                 print("there arn't that many books availabe")
 
     def return_books(self):
-        if self.borrowed_books == []:
+        if len(self.borrowed_books) == 0:
             print("you havent borrowed any book yet")
         else:
             print("these are the books you have borrowed - ")
-            for i, book in enumerate(self.borrowed_books):
-                print(f"{i+1}. {book.title()}")
+            for index, book in self.borrowed_books.items():
+                if book[1] == "returned":
+                    continue
+                else:
+                    print(f"{index} - {book[0]}")
             no_of_return = int(input("How many books would you like to return: "))
-            if no_of_return <= len(self.borrowed_books):
+            borrowed_book = [k for k, v in self.list_of_books.items() if v[1] == "borrowed"]
+            if no_of_return <= len(borrowed_book):
                 print("enter the number specific to the book you want to return....")
-                indexes = []
                 for i in range(1, no_of_return+1):
-                    returned_book = int(input("--> ")) - 1
-                    indexes.append(returned_book)
-                for index in sorted(indexes, reverse= True):
-                    book = self.borrowed_books[index]
-                    print(f"you have succesfuly returned {book}")
-                    self.list_of_books.append(book)
-                    self.borrowed_books.remove(book)
+                    index = int(input("--> "))
+                    if self.borrowed_books[index][1] == "returned":
+                        print(f"you have already returned that book")
+                    else:
+                        print(f"you have successfully returned {self.borrowed_books[index][0]}")
+                        self.list_of_books[index][1] = "available"
+                        self.borrowed_books[index][1] = "returned"
             else:
                 print("you havnt borrowed that many books")
 
@@ -109,5 +127,7 @@ def libraryExecution():
             print("please give valid response...")
         except IndexError:
             print("please give valid response...")
+        except KeyError:
+            print("please give valid response")
 
 libraryExecution()
